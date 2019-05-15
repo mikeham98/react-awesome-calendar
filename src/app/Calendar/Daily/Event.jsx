12d2a0af55957d5b2a57baf75a485bbcf3927a01
Michael Hamilton <98mikeham@gmail.com>
2019-05-15 23:16:40 +0100

    stopped propagation of clicking on event

--- src/app/Calendar/Daily/Event.jsx
+++ src/app/Calendar/Daily/Event.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import styles from './Event.styles.scss';
 
 export default class Event extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(e) {
+    e.stopPropagation();
+    this.props.onClick();
+  }
+
   render() {
     return (
       <div
         style={{ backgroundColor: this.props.color }}
-        onClick={this.props.onClick}
+        onClick={this.onClick}
         className={styles.dailyEventWrapper}
       >
         <span className={styles.dailyEventTitle}>{this.props.title}</span>
